7f76af2d30ec921ba09b25e12544ae7ef4e74817
Toru Kuriyama <paprapapra@gmail.com>
2024-02-04 15:38:30 +0900

    Add new Xcode project.

---
+++ SwiftMarkdownSample/SimpleMarkdown/SimpleMarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMarkdownApp.swift
+//  SimpleMarkdown
+//
+//  Created by 栗山徹 on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
